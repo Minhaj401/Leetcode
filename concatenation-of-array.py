ce8ca6c2f73832ed744db222ceb3815ea9c14bd5
Minhaj401 <minhajnoushad123@gmail.com>
2025-04-29 19:09:43 +0530

    _

---
+++ concatenation-of-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums1=nums+nums
+        return nums1
