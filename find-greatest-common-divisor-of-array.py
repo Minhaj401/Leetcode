0f1b6861bf997fb706b1f496efacc0b0c79debbf
Minhaj401 <minhajnoushad123@gmail.com>
2025-05-05 23:40:06 +0530

    .

---
+++ find-greatest-common-divisor-of-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findGCD(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        smallest = min(nums)
+        largest = max(nums)
+        return gcd(smallest, largest)
