d509d069ae9db87bde3d22d298467bd896801307
Minhaj401 <minhajnoushad123@gmail.com>
2025-05-08 18:06:16 +0530

    .

---
+++ richest-customer-wealth.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        max=0
+        for i in accounts:
+            if sum(i)>max:
+                max=sum(i)
+        return max
