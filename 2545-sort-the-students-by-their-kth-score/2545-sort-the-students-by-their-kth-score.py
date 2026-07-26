fc8792f82bded203cc4de211fc0aa7f60b3839fc
Minhaj Noushad <105638853+Minhaj401@users.noreply.github.com>
2026-07-26 16:06:53 +0530

    Time: 12 ms (8.66%), Space: 25.1 MB (39.64%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        n = len(score)
+        for i in range(n - 1):
+            for j in range(n - 1 - i):
+                if score[j][k] < score[j + 1][k]:
+                    score[j], score[j + 1] = score[j + 1], score[j]
+
+        return score
